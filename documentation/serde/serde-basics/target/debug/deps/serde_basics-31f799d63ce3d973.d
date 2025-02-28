d56e6b4f3f26ade358f6a0bcfda541a88763cf8c
GoodGuyGregory <greg.witt625@gmail.com>
2025-02-27 16:44:21 -0800

    adds counter-app project

---
+++ documentation/serde/serde-basics/target/debug/deps/serde_basics-31f799d63ce3d973.d
@@ -0,0 +1,5 @@
+/Users/gregwitt/Github/vinyl-swarm/documentation/serde/serde-basics/target/debug/deps/libserde_basics-31f799d63ce3d973.rmeta: src/main.rs
+
+/Users/gregwitt/Github/vinyl-swarm/documentation/serde/serde-basics/target/debug/deps/serde_basics-31f799d63ce3d973.d: src/main.rs
+
+src/main.rs:
---
+++ documentation/serde/serde-basics/target/debug/deps/serde_basics-36fd816a16148500.d
@@ -0,0 +1,5 @@
+/Users/gregwitt/Github/vinyl-swarm/documentation/serde/serde-basics/target/debug/deps/libserde_basics-36fd816a16148500.rmeta: src/main.rs
+
+/Users/gregwitt/Github/vinyl-swarm/documentation/serde/serde-basics/target/debug/deps/serde_basics-36fd816a16148500.d: src/main.rs
+
+src/main.rs:
