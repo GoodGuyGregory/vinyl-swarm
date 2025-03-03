6e8f920d32b29d9a4550cf8316393d4865e36e94
GoodGuyGregory <greg.witt625@gmail.com>
2025-03-03 06:12:34 -0800

    adds documentation counter-app changes

---
+++ documentation/counter-app/target/debug/deps/counter_app-fb85c19dccb0a031.d
@@ -0,0 +1,5 @@
+/Users/gregwitt/Github/vinyl-swarm/documentation/counter-app/target/debug/deps/libcounter_app-fb85c19dccb0a031.rmeta: src/main.rs
+
+/Users/gregwitt/Github/vinyl-swarm/documentation/counter-app/target/debug/deps/counter_app-fb85c19dccb0a031.d: src/main.rs
+
+src/main.rs:
---
+++ documentation/counter-app/target/debug/deps/rustc_demangle-561abddba9f53661.d
@@ -0,0 +1,7 @@
+/Users/gregwitt/Github/vinyl-swarm/documentation/counter-app/target/debug/deps/librustc_demangle-561abddba9f53661.rmeta: /Users/gregwitt/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rustc-demangle-0.1.24/src/lib.rs /Users/gregwitt/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rustc-demangle-0.1.24/src/legacy.rs /Users/gregwitt/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rustc-demangle-0.1.24/src/v0.rs
+
+/Users/gregwitt/Github/vinyl-swarm/documentation/counter-app/target/debug/deps/rustc_demangle-561abddba9f53661.d: /Users/gregwitt/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rustc-demangle-0.1.24/src/lib.rs /Users/gregwitt/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rustc-demangle-0.1.24/src/legacy.rs /Users/gregwitt/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rustc-demangle-0.1.24/src/v0.rs
+
+/Users/gregwitt/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rustc-demangle-0.1.24/src/lib.rs:
+/Users/gregwitt/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rustc-demangle-0.1.24/src/legacy.rs:
+/Users/gregwitt/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rustc-demangle-0.1.24/src/v0.rs:
