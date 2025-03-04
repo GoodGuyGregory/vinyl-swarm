1c05dbc347e983fdd4fbcdf1528ee02d55cb164a
GoodGuyGregory <greg.witt625@gmail.com>
2025-03-04 13:50:51 -0800

    adds axum and postgres integrations

--- documentation/axum/rust-axum-postgres-api/src/main.rs
+++ documentation/axum/rust-axum-postgres-api/src/main.rs
@@ -1,7 +1,17 @@
 use axum::{response::IntoResponse, routing::get, Json, Router};
+use dotenv::dotenv;
+use sqlx::{postgres::PgPoolOptions, PgPool, Postgres, Pool};
+use std::env;
+use std::error::Error;
+use std::sync::Arc;
+
+
+pub struct AppState {
+    db: Pool<Postgres>,
+}
 
 /// basic status check endpoint
-async fn health_check_handler() -> impl IntoResponse {
+async fn status_check_handler() -> impl IntoResponse {
     const MESSAGE: &str = "Simple CRUD API with Rust, SQLX, Postgres, and Axum";
 
     let json_response = serde_json::json!({
@@ -14,15 +24,41 @@ async fn health_check_handler() -> impl IntoResponse {
 }
 
 
+async fn connect_to_database() -> Result<PgPool, Box<dyn Error>> {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    
+    let pool = PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url)
+        .await?;
+
+        
+    println!("✅ Connection to the database is successful!");
+    Ok(pool)
+}
+
 /// the main API logic 
 #[tokio::main]
 async fn main() {
-    // create the app
-    let app = Router::new().route("/api/healthchecker", get(health_check_handler));
-    
-    println!("🛸 Server started successfully");
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    match connect_to_database().await {
+        Ok(pool) => {
+            let app_state = Arc::new(AppState { db: pool.clone() });
+              // create the app
+            let app = Router::new().route("/api/status", get(status_check_handler));
+            
+            println!("🛸 Server started successfully");
 
-    axum::serve(listener, app).await.unwrap();
+            let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+
+            axum::serve(listener, app).await.unwrap();
+        }
+        // catch the Error
+        Err(err) => {
+            println!("🔥 Failed to connect to the database: {:?}", err);
+        }
+    }
+
 }
--- vinyl-swarm/src/main.rs
+++ vinyl-swarm/src/main.rs
@@ -1,8 +1,17 @@
 use axum::{response::IntoResponse, routing::get, Json, Router};
 use tokio::net::TcpListener;
+use sqlx::{postgres::PgPoolOptions, PgPool, Postgres, Pool};
+use dotenv::dotenv;
+use std::env;
+use std::error::Error;
+use std::sync::Arc;
+
+pub struct AppState {
+    db: Pool<Postgres>,
+}
 
 pub async fn status_handler() -> impl IntoResponse {
-    let message_status: &str = "Vinyl Swarm Running";
+    let message_status: &str = "vinyl swarm running: 👽 ";
 
     let json_response = serde_json::json!({
         "status": "ok",
@@ -12,18 +21,41 @@ pub async fn status_handler() -> impl IntoResponse {
     Json(json_response)
 }
 
+async fn connect_to_database() -> Result<PgPool, Box<dyn Error>> {
+    dotenv().ok();
 
-#[tokio::main]
-async fn main() {
-    let app = Router::new().route("/api/status", get(status_handler) );
+    let database_url = env::var("DATABASE_URL").expect("ERROR: DATABASE_URL must be set");
+    
+    let pool = PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url)
+        .await?;
 
-    println!("Server started successfully at localhost:8080");
-
-    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
-
-    axum::serve(listener, app.into_make_service())
-        .await
-        .unwrap();
+    println!("✅ Connection to the database is successful!");
+    Ok(pool)
 }
 
 
+
+// Basics of using AXUM with mySQL
+#[tokio::main]
+async fn main() {
+    
+    match connect_to_database().await {
+        Ok(pool) => {
+            let app_state = Arc::new(AppState { db: pool.clone() });
+              // create the app
+            let app = Router::new().route("/api/status", get(status_handler));
+            
+            println!("🛸 Server started successfully");
+
+            let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+
+            axum::serve(listener, app).await.unwrap();
+        }
+        // catch the Error
+        Err(err) => {
+            println!("🔥 Failed to connect to the database: {:?}", err);
+        }
+    }
+}
