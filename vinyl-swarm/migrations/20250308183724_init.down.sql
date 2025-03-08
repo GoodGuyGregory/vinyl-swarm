992b55bdaec52ef14f6d77b6df52cac7a18d021b
GoodGuyGregory <greg.witt625@gmail.com>
2025-03-08 14:16:32 -0800

    adds down migration script

---
+++ vinyl-swarm/migrations/20250308183724_init.down.sql
@@ -0,0 +1,17 @@
+-- Add down migration script here
+
+-- delete the users table
+DROP TABLE IF EXISTS users;
+
+-- delete the records table
+DROP TABLE IF EXISTS records;
+
+-- delete the record-stores table
+DROP TABLE IF EXISTS record_stores;
+
+-- delete the user_record_stores table 
+DROP TABLE IF EXISTS user_record_stores;
+
+-- delte the user_records table
+DROP TABLE IF EXISTS user_records;
+
