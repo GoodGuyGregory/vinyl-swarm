d40ef69a16bfa0bb1d08cf45e1812ab09cd410d1
GoodGuyGregory <greg.witt625@gmail.com>
2025-03-08 22:29:31 -0800

    adds router with status and app state create_router and future proofs additional routes for api

---
+++ vinyl-swarm/src/routes/router.rs
@@ -0,0 +1,36 @@
+use std::sync::Arc;
+use axum::{
+    routing::{get},
+    Router,
+    Json,
+    response::IntoResponse,
+};
+
+// internal modules 
+use crate::AppState;
+
+pub async fn status_handler() -> impl IntoResponse {
+    let message_status: &str = "vinyl swarm running: 👽 ";
+
+    let json_response = serde_json::json!({
+        "status": "ok",
+        "message": message_status
+    });
+
+    Json(json_response)
+}
+
+// api documentation:
+// https://docs.rs/crate/axum/latest/source/src/docs/routing/nest.md
+pub fn create_router(app_state: Arc<AppState>) -> Router {
+    // create the router for all CRUD endpoints
+    let api_routes = Router::new()
+    // status route
+        .route("/status", get(status_handler));
+
+    // return the router
+    Router::new()
+        .nest("/api", api_routes)
+        .with_state(app_state)
+
+}
