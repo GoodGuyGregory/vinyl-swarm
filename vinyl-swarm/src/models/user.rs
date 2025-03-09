850171b03db5a6b854ca9be924139c3a902ca22a
GoodGuyGregory <greg.witt625@gmail.com>
2025-03-09 14:05:34 -0700

    adds user model to prevent private details being leaked

--- vinyl-swarm/src/models/user.rs
+++ vinyl-swarm/src/models/user.rs
@@ -1,14 +1,46 @@
-use chrono::NaiveDateTime;
+use chrono::{DateTime, Utc};
 use serde::{Serialize, Deserialize};
 use uuid::Uuid;
 
+
+/// for pagination in a front end UI
+#[derive(Deserialize, Debug, Default)]
+pub struct FilterOptions {
+    pub page: Option<usize>,
+    pub limit: Option<usize>,
+}
+
+// due to security concerns
+#[derive(Serialize, Deserialize, Debug)]
+pub struct UserResponseSchema {
+    pub user_name: String,
+    pub user_first_name: String,
+    pub user_last_name: String,
+    pub user_email: String,
+    pub created_at: Option<DateTime<Utc>>,
+}
+
+// helps for converting to the appropriate exposed 
+// API GET Endpoints leveraging the From trait 
+impl From<UserModel> for UserResponseSchema {
+    fn from(user: UserModel) -> Self {
+        UserResponseSchema {
+            user_name: user.user_name,
+            user_first_name: user.user_first_name,
+            user_last_name: user.user_last_name,
+            user_email: user.user_email,
+            created_at: user.created_at,
+        }
+    }
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct UserModel {
     pub user_id: Uuid,
     pub user_name: String,
     pub user_first_name: String,
+    pub user_last_name: String,
     pub user_email: String,
     pub user_password: String,
-    pub created_at: NaiveDateTime,
+    pub created_at: Option<DateTime<Utc>>,
 }
