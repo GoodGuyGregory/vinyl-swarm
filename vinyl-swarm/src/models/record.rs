d3284a9f4007fced41e3aa5d7945265d45c23772
GoodGuyGregory <greg.witt625@gmail.com>
2025-03-08 21:00:10 -0800

    adds record.rs

---
+++ vinyl-swarm/src/models/record.rs
@@ -0,0 +1,15 @@
+use chrono::{NaiveDate, NaiveTime},
+use serde::{Serialize, Deserialize},
+use uuid::Uuid;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RecordModel {
+    pub record_id: Uuid,
+    pub artist: String,
+    pub title: String,
+    pub released: NaiveTime,
+    pub format: String,
+    pub price: f64,
+    pub label: String,
+    pub duration_length: NaiveTime,
+}
