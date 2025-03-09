72a3593b05a2e410105032df1c8604496bfb022d
GoodGuyGregory <greg.witt625@gmail.com>
2025-03-09 00:32:17 -0800

    adds mod record_stores and records and users to the project and initial creates the records get method

--- vinyl-swarm/src/routes/mod.rs
+++ vinyl-swarm/src/routes/mod.rs
@@ -1,3 +1,6 @@
 pub mod router;
+pub mod records;
+pub mod users;
+pub mod record_stores;
 
 pub use router::create_router;
--- vinyl-swarm/src/routes/router.rs
+++ vinyl-swarm/src/routes/router.rs
@@ -1,13 +1,17 @@
 use std::sync::Arc;
 use axum::{
-    routing::{get},
+    routing::get,
     Router,
     Json,
     response::IntoResponse,
 };
 
+
 // internal modules 
-use crate::AppState;
+use crate::{
+    AppState,
+    routes::records::list_all_records,
+};
 
 pub async fn status_handler() -> impl IntoResponse {
     let message_status: &str = "vinyl swarm running: 👽 ";
@@ -26,7 +30,8 @@ pub fn create_router(app_state: Arc<AppState>) -> Router {
     // create the router for all CRUD endpoints
     let api_routes = Router::new()
     // status route
-        .route("/status", get(status_handler));
+        .route("/status", get(status_handler))
+        .route("/records", get(list_all_records));
 
     // return the router
     Router::new()
