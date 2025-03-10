24726a4143e33c00c880ba95ca5ee00b5f9a0dd6
GoodGuyGregory <greg.witt625@gmail.com>
2025-03-09 21:59:57 -0700

    adds user model

--- vinyl-swarm/src/models/user.rs
+++ vinyl-swarm/src/models/user.rs
@@ -11,15 +11,10 @@ pub struct FilterOptions {
 }
 
 
-// used to search for a single username
-#[derive(Deserialize, Debug)]
-pub struct ParamOptions {
-    pub user_name: String,
-}
-
 // due to security concerns
 #[derive(Serialize, Deserialize, Debug)]
 pub struct UserResponseSchema {
+    pub user_id: Uuid,
     pub user_name: String,
     pub user_first_name: String,
     pub user_last_name: String,
@@ -36,11 +31,21 @@ pub struct CreateUserSchema {
     pub user_password: String,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct UpdateUserSchema {
+    pub user_name: Option<String>,
+    pub user_first_name: Option<String>,
+    pub user_last_name: Option<String>,
+    pub user_email: Option<String>,
+    pub user_password: Option<String>,
+}
+
 // helps for converting to the appropriate exposed 
 // API GET Endpoints leveraging the From trait 
 impl From<UserModel> for UserResponseSchema {
     fn from(user: UserModel) -> Self {
         UserResponseSchema {
+            user_id: user.user_id,
             user_name: user.user_name,
             user_first_name: user.user_first_name,
             user_last_name: user.user_last_name,
