289d9f90b426fa72be618cd70fb7925e9cbde899
GoodGuyGregory <greg.witt625@gmail.com>
2025-03-10 00:18:29 -0700

    adds users route for getting user_records, and removing user_records

--- vinyl-swarm/src/handlers/mod.rs
+++
@@ -1,1 +0,0 @@
-pub use status_handler::status_handler;
--- vinyl-swarm/src/routes/router.rs
+++ vinyl-swarm/src/routes/router.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 use axum::{
     response::IntoResponse, 
-    routing::{get, patch}, 
+    routing::{get},
     Json, 
     Router
 };
@@ -10,9 +10,11 @@ use axum::{
 // internal modules 
 use crate::{
     AppState,
-    routes::records::list_all_records,
+    routes::records::{list_all_records, find_record, create_new_record, edit_record, delete_record_by_id},
     routes::record_stores::{list_all_stores, create_record_store, edit_record_store, find_record_store, delete_record_store},
-    routes::users::{list_all_users, find_specific_user, create_user, edit_user, delete_user},
+    routes::users::{
+        list_all_users, find_specific_user, create_user, edit_user, delete_user, 
+        get_user_records, remove_all_user_records},
 };
 
 pub async fn status_handler() -> impl IntoResponse {
@@ -33,7 +35,11 @@ pub fn create_router(app_state: Arc<AppState>) -> Router {
     let api_routes = Router::new()
     // status route
         .route("/status", get(status_handler))
-        .route("/records", get(list_all_records))
+        .route("/records", get(list_all_records)
+                                            .post(create_new_record))
+        .route("/records/{id}", get(find_record)
+                                                    .patch(edit_record)
+                                                    .delete(delete_record_by_id))
         .route("/stores", get(list_all_stores)
                                             .post(create_record_store))
         .route("/stores/{id}",    
@@ -45,7 +51,16 @@ pub fn create_router(app_state: Arc<AppState>) -> Router {
                     .post(create_user))
         .route("/users/{id}", get(find_specific_user)
                             .patch(edit_user)
-                        .delete(delete_user));
+                        .delete(delete_user))
+        .route("/users/records/{user_id}", get(get_user_records)
+                                                                // .post(create_user_record)
+                                                                .delete(remove_all_user_records));
+        // .route("/users/wishlist/{user_id}" get(get_users_wishlist)
+        //                                             .post(add_to_user_wishlist)
+        //                                         .delete(remove_from_user_wishlist))
+        // .route("/users/record_stores/{user_id}", get(get_user_record_stores)
+        //                                                     .post(add_user_record_store)
+        //                                                     .delete(delete_user_record_store));
 
     // return the router
     Router::new()
