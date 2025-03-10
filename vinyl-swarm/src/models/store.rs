06edd3eef0a7152fdae6d54f3288d77c895377df
GoodGuyGregory <greg.witt625@gmail.com>
2025-03-09 19:42:51 -0700

    adds store.rs

--- vinyl-swarm/src/models/store.rs
+++ vinyl-swarm/src/models/store.rs
@@ -1,8 +1,38 @@
 use serde::{Serialize, Deserialize};
 use uuid::Uuid;
 
+
+/// for pagination in a front end UI
+#[derive(Deserialize, Debug, Default)]
+pub struct FilterOptions {
+    pub page: Option<usize>,
+    pub limit: Option<usize>,
+}
+
 #[derive(Debug, Serialize, Deserialize)]
-pub struct RecordStore {
+pub struct CreateRecordStoreSchema {
+    pub store_name: String,
+    pub store_address: String,
+    pub store_city: String,
+    pub store_state: String,
+    pub store_zip: String,
+    pub phone_number: Option<String>,
+    pub website: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct UpdateRecordStoreSchema {
+    pub store_name: Option<String>,
+    pub store_address: Option<String>,
+    pub store_city: Option<String>,
+    pub store_state: Option<String>,
+    pub store_zip: Option<String>,
+    pub phone_number: Option<String>,
+    pub website: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RecordStoreModel {
     pub record_store_id: Uuid,
     pub store_name: String,
     pub store_address: String,
