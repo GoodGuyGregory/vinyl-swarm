7a2e7410984107673e69b8d8bd3ef3c7f3a3cbe4
GoodGuyGregory <greg.witt625@gmail.com>
2025-03-09 21:50:49 -0700

    adds create record schema, update record schema

--- vinyl-swarm/src/models/record.rs
+++ vinyl-swarm/src/models/record.rs
@@ -9,6 +9,35 @@ pub struct FilterOptions {
     pub limit: Option<usize>,
 }
 
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CreateRecordSchema {
+    pub artist: String,
+    pub title: String,
+    pub released: NaiveDate,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub genre: Option<Vec<String>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub format: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub price: Option<BigDecimal>,
+    pub label: String,
+    pub duration_length: NaiveTime,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct UpdateRecordSchema {
+    pub artist: Option<String>,
+    pub title: Option<String>,
+    pub released: Option<NaiveDate>,
+    pub genre: Option<Vec<String>>,
+    pub format: Option<String>,
+    pub price: Option<BigDecimal>,
+    pub label: Option<String>,
+    pub duration_length: Option<NaiveTime>,
+}
+
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct RecordModel {
     pub record_id: Uuid,
