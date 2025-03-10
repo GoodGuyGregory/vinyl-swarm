f20e932e33b54dbaab599ad97f51e03af6f49bce
GoodGuyGregory <greg.witt625@gmail.com>
2025-03-09 18:30:05 -0700

    adds record_store adds POST

--- vinyl-swarm/src/routes/record_stores.rs
+++ vinyl-swarm/src/routes/record_stores.rs
@@ -1,14 +1,12 @@
 use std::sync::Arc;
 
 use axum::{
-    extract::{Query, State},
-    http::StatusCode,
-    response::IntoResponse,
-    Json,
+    body, extract::{Query, State}, http::StatusCode, response::IntoResponse, Json
 };
 use chrono::offset;
+use serde_json::json;
 use crate::AppState;
-use crate::models::store::{RecordStoreModel, FilterOptions};
+use crate::models::store::{RecordStoreModel, FilterOptions, CreateRecordStoreSchema};
 
 
 /// GET all record stores from the database
@@ -50,4 +48,47 @@ pub async fn list_all_stores(
             (StatusCode::INTERNAL_SERVER_ERROR, Json(error_response))
         }
     }
+}
+
+/// create_record_store
+/// this handler is used for adding additional record stores that are worth shopping at.
+/// POST methods are recommended for this handler
+pub async fn create_record_store(
+    State(data): State<Arc<AppState>>,
+    Json(body): Json<CreateRecordStoreSchema>
+) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
+
+    // create the insert statement to add another record store
+    let query_result = sqlx::query_as!(
+        RecordStoreModel, 
+        "INSERT INTO record_stores (store_name, store_address, store_city, store_state, store_zip, phone_number, website)
+        VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+        body.store_name.to_string(),
+        body.store_address.to_string(),
+        body.store_city.to_string(),
+        body.store_state.to_string(),
+        body.store_zip.to_string(),
+        body.phone_number.to_owned().unwrap_or("".to_string()),
+        body.website.to_owned().unwrap_or("".to_string())
+    ).fetch_one(&data.db)
+    .await;  
+
+    match query_result {
+        Ok(created_record_store) => {
+            let record_store_response = json!({
+                "status": "success",
+                "record_store": created_record_store
+            });
+
+            return Ok((StatusCode::CREATED, Json(record_store_response)));
+        }
+        Err(e) => {
+            // otherwise it's not a duplicate and something went wrong
+            return Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(json!({"status": "error", "message": format!("{:?}", e)}))
+            ));
+        }
+    }
+
 }
