6ca5afb8940224e8602e7e496b73de865a25b864
GoodGuyGregory <greg.witt625@gmail.com>
2025-03-11 20:16:12 -0700

    adds migrations and adds routes and src changes to support duplication preventions

--- vinyl-swarm/migrations/20250308183724_init.down.sql
+++ vinyl-swarm/migrations/20250308183724_init.down.sql
@@ -1,19 +1,19 @@
 -- Add down migration script here
 
 -- delete the users table
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
 
 -- delete the records table
-DROP TABLE IF EXISTS records;
+DROP TABLE IF EXISTS records CASCADE;
 
 -- delete the record-stores table
-DROP TABLE IF EXISTS record_stores;
+DROP TABLE IF EXISTS record_stores CASCADE;
 
 -- delete the user_record_stores table 
-DROP TABLE IF EXISTS user_record_stores;
+DROP TABLE IF EXISTS user_record_stores CASCADE;
 
 -- delete the user_records table
-DROP TABLE IF EXISTS user_records;
+DROP TABLE IF EXISTS user_records CASCADE;
 
 -- delete the user_wishlist table
-DROP TABLE IF EXISTS user_wishlist;
+DROP TABLE IF EXISTS user_wishlist CASCADE;
